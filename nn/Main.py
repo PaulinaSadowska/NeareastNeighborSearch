9a6e9cb68ca3045e60d488f2b256397e2d0eef3a
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-01-11 14:59:14 +0100

    create map of users and unique song ids

--- nn/Main.py
+++ nn/Main.py
@@ -5,18 +5,27 @@ before = d.datetime.now()
 file_paths = fu.read_json("../paths.json")
 
 input_path = file_paths["folderPath"] + file_paths["dataPath"]
+input_path_short = file_paths["folderPath"] + file_paths["shortDataPath"]
 output_path = file_paths["folderPath"] + file_paths["outputPath"]
 expected_output_path = file_paths["folderPath"] + file_paths["expectedOutputPath"]
 
-print(input_path)
-print(output_path)
 users = {}
-with fu.open_file(input_path) as f:
+with fu.open_file(input_path_short) as f:
+    i = 0
     for line in f:
-        userId = line.split(",")[1]
-        users[userId] = 1
+        if i == 0:  # ignore first line
+            i += 1
+            continue
+        data = line.split(",")
+        userId = data[1]
+        songId = data[0]
+        if userId in users:
+            if songId not in users[userId]:
+                users[userId].append(songId)
+        else:
+            users[userId] = [songId]
 
-print(len(users))
+for u in users:
+    print(u + " {0}".format(len(users[u])))
 
 print("executed in {0}".format(d.datetime.now() - before))
-
